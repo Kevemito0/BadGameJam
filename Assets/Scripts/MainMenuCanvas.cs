22d6345d3ed031cf70759d4ff6970b6a0f5ee4a8
Kevem <azyedekarambital@gmail.com>
2026-06-20 14:26:54 +0300

    Initial commit

---
+++ Assets/Scripts/MainMenuCanvas.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button settingsBtn;
+    [SerializeField] private Button quitBtn;
+
+    [Header("Panels")]
+    [SerializeField] private GameObject settingsPanel;
+
+    [Header("Scene")]
+    [SerializeField] private string gameSceneName = "GameScene";
+
+    private void Start()
+    {
+        playBtn.onClick.AddListener(OnPlayClicked);
+        settingsBtn.onClick.AddListener(OnSettingsClicked);
+        quitBtn.onClick.AddListener(OnQuitClicked);
+
+        settingsPanel.SetActive(false);
+    }
+
+    private void OnPlayClicked()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    private void OnSettingsClicked()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    private void OnQuitClicked()
+    {
+        Debug.Log("Quiting Game");
+        Application.Quit();
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+}
